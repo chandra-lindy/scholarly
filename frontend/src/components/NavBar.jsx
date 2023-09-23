f4ef70e6ce062a495ff2efcb9c52657d0f2d2094
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-09-23 07:25:27 -0700

    reduced navbar thickness

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,13 +4,17 @@ const NavBar = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bg-brand-paper z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-brand-aux-1 font-bold text-4xl">
-          <img className="h-[4.75rem] w-auto inline-block" src={logo} alt="" />
+        <div className="text-brand-aux-1 font-bold text-2xl">
+          <img className="h-[3.75rem] w-auto inline-block" src={logo} alt="" />
           Scholarly
         </div>
         <div>
-          <button className="mr-14 text-brand-aux-1 text-xl">Register</button>
-          <button className="text-brand-aux-1 text-xl">Login</button>
+          <button className="mr-14 text-brand-aux-1 hover:text-brand-aux-2 text-l">
+            Register
+          </button>
+          <button className="text-brand-aux-1 hover:text-brand-aux-2 text-l">
+            Login
+          </button>
         </div>
       </div>
     </div>
