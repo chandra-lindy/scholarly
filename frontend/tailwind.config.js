7d491ad105fd777716ce6f85b824940786435b9e
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-09-23 07:58:31 -0700

    changed brand-paper color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         "brand-main": "#c8532c",
         "brand-aux-1": "#dc4304",
         "brand-aux-2": "#f64f06",
-        "brand-paper": "#f7f7f7",
+        "brand-paper": "#fdf6e3",
       },
     },
   },
