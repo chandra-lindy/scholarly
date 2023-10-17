72a67dc28675a5aa5f695471f806db0782731abb
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-16 23:02:39 -0700

    removed debugging related code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,6 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  console.log("useNavigate: ", useNavigate);
   const navigate = useNavigate();
   const location = useLocation();
   const errorMessage = location.state?.error || "";
