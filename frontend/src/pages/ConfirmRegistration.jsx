37ebb7c19bf2a7b1d1c3ece616c8a03307eda35f
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-16 22:42:10 -0700

    display error message

--- frontend/src/pages/ConfirmRegistration.jsx
+++ frontend/src/pages/ConfirmRegistration.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Auth } from "aws-amplify";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import logo from "../assets/logo-no-slogan.png";
 import { Link } from "react-router-dom";
 
@@ -9,14 +9,15 @@ const ConfirmRegistration = () => {
   const [code, setCode] = useState("");
 
   const navigate = useNavigate();
+  const location = useLocation();
+  const errorMessage = location.state?.error || "";
 
   const confirmSignUp = async () => {
     try {
       await Auth.confirmSignUp(email, code);
-      console.log("User confirmed");
       navigate("/dashboard");
     } catch (error) {
-      console.log("Error confirming user", error);
+      navigate("/confirm", { state: { error: error.message } });
     }
   };
 
@@ -26,6 +27,12 @@ const ConfirmRegistration = () => {
         <Link to="/">
           <img src={logo} alt="Scholarly logo" className="max-w-md mb-12" />
         </Link>
+        <p
+          className="p-2 my-4 text-red-600 text-xs text-center"
+          style={{ visibility: errorMessage ? "visible" : "hidden" }}
+        >
+          {errorMessage || "Error message if any"}
+        </p>
         <input
           type="text"
           placeholder="Email"
