cf50759a890e3ad20c7dd7102433b3ce40becff9
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-16 23:03:18 -0700

    fixed padding breakpoints

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -3,10 +3,10 @@ import openBook from "../assets/open-book.png";
 const FileUpload = () => {
   return (
     <div className="flex bg-brand-aux-2 w-full opacity-25 border-4 border-black rounded-lg border-dashed">
-      <div className="w-1/2 p-10 lg:p-5">
+      <div className="w-1/2 p-5">
         <img src={openBook} alt="Icon - Opened Book" />
       </div>
-      <div className="w-1/2 lg:p-1 lg:py-6 lg:pr-4 lg:text-xs">
+      <div className="w-1/2 xl:pt-6 xl:pr-4 xl:text-xs 2xl:pt-10 2xl:pr-4">
         <div className="text-center text-base text-white">
           Drag and drop .pdf files to upload
         </div>
