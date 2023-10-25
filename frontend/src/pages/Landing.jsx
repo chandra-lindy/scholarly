7b392e4755cae2ba941426dfa20b7c6e501f395f
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-25 11:15:38 -0700

    test front and backend integration

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,25 @@
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 
+import axios from "axios";
+import { useEffect } from "react";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
 const Landing = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(`${BACKEND_URL}/`);
+        console.log("process.env: ", process.env);
+        const response = await axios.get(`${BACKEND_URL}/`);
+        console.log("response: ", response.data);
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="flex flex-col">
       <NavBar />
