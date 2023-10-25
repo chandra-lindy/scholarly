507d210bafe0f3edd1ad4d01f2f98f548c7004b7
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-25 14:49:22 -0700

    frontend - backend integration completed

--- backend/main.py
+++ backend/main.py
@@ -8,32 +8,23 @@ load_dotenv()
 aws_access_key = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 aws_session_token = os.environ.get("AWS_SESSION_TOKEN")
+use_nginx_cors = os.environ.get("USE_NGINX_CORS")
 
 app = FastAPI()
 
-origins = [
-  "https://scholarly-ai.com",
-  os.environ.get("FRONTEND_URL"),
-]
+if use_nginx_cors:
+    origins = [
+        os.environ.get("FRONTEND_URL"),
+    ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.middleware("http")
-async def log_middleware(request, call_next):
-    response = await call_next(request)
-    print(response.headers)
-    return response
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 @app.get("/")
 async def root():
     return {"message": "World! Ready or not ... Here I come!!"}
-
-@app.get("/login")
-async def login():
-    return {"message": "Login"}
