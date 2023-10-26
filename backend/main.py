8af60f7ba2b0fa12fbe968433deb18c68004c4c6
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-26 16:45:23 -0700

    complete definition of get_current_user function

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@
 # standard library imports
 from dotenv import load_dotenv
 import os
+import requests
 
 # third party imports
 from fastapi import FastAPI, Depends, HTTPException, Security
@@ -19,6 +20,9 @@ aws_session_token = os.environ.get("AWS_SESSION_TOKEN")
 use_nginx_cors = bool(int(os.environ.get("USE_NGINX_CORS")))
 secure_flag = bool(int(os.environ.get("SECURE_FLAG")))
 jwt_algorithm = os.environ.get("JWT_ALGORITHM")
+jwt_audience = os.environ.get("JWT_AUDIENCE")
+jwt_issuer = os.environ.get("JWT_ISSUER")
+cognito_jwks_url = os.environ.get("COGNITO_JWKS_URL")
 
 app = FastAPI()
 
@@ -38,13 +42,44 @@ if not use_nginx_cors:
 
 security = HTTPBearer()
 
+# get jwks from cognito
+jwks_cache = None
+def fetch_jwks() -> list:
+    global jwks_cache
+    if jwks_cache is None:
+        url = cognito_jwks_url
+        response = requests.get(url)
+        jwks_cache = response.json()["keys"]
+    return jwks_cache
+
 def get_current_user(authorization: HTTPAuthorizationCredentials = Security(security)):
     token = authorization.credentials
+
+    jwks = fetch_jwks()
+    header = jwt.get_unverified_header(token)
+    rsa_key = {}
+
+    for key in jwks:
+        if key["kid"] == header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "alg": key["alg"],
+                "n": key["n"],
+                "e": key["e"],
+            }
+
     try:
         payload = jwt.decode(
             token,
-            altorithms=[jwt_algorithm]
+            rsa_key,
+            algorithms=[jwt_algorithm],
+            audience=jwt_audience,
+            issuer=jwt_issuer,
         )
+        # debug code
+        print('payload: ', payload)
         user = payload.get("sub")
         if user is None:
             raise HTTPException(status_code=403, detail="User not found")
@@ -54,7 +89,7 @@ def get_current_user(authorization: HTTPAuthorizationCredentials = Security(secu
 
 @app.get("/chat")
 async def chat_route(current_user: str = Depends(get_current_user)):
-    return {"message": "Welcome to the chat!"}
+    return {"message": "Welcome to the chat!", "user": current_user}
 
 @app.get("/")
 async def root():
