be3cf83ce85e5e8c989c0fc036aaff8e55da72cd
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-10-26 17:36:23 -0700

    add protected /chat route

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ secure_flag = bool(int(os.environ.get("SECURE_FLAG")))
 jwt_algorithm = os.environ.get("JWT_ALGORITHM")
 jwt_audience = os.environ.get("JWT_AUDIENCE")
 jwt_issuer = os.environ.get("JWT_ISSUER")
-cognito_jwks_url = os.environ.get("COGNITO_JWKS_URL")
+cognito_jwk_url = os.environ.get("COGNITO_JWK_URL")
 
 app = FastAPI()
 
@@ -47,7 +47,7 @@ jwks_cache = None
 def fetch_jwks() -> list:
     global jwks_cache
     if jwks_cache is None:
-        url = cognito_jwks_url
+        url = cognito_jwk_url
         response = requests.get(url)
         jwks_cache = response.json()["keys"]
     return jwks_cache
@@ -87,7 +87,7 @@ def get_current_user(authorization: HTTPAuthorizationCredentials = Security(secu
     except JWTError:
         raise HTTPException(status_code=403, detail="Invalid Token")
 
-@app.get("/chat")
+@app.post("/chat")
 async def chat_route(current_user: str = Depends(get_current_user)):
     return {"message": "Welcome to the chat!", "user": current_user}
 
