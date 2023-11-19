5e9c38e6970eb1b345ac247f032cceba5efc8851
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-11-18 17:02:01 -0800

    disabled word wrap to book icons and a fade effect when overflow is triggered

--- frontend/src/utils/Card.jsx
+++ frontend/src/utils/Card.jsx
@@ -6,16 +6,16 @@ const Card = ({ type, title, handleSelectFile }) => {
     type === "book" ? (
       <img className="h-[1.5rem] pr-4" src={bookIcon} alt="Icong - Open Book" />
     ) : (
-      "???"
+      "???" // other types goes here
     );
 
   return (
     <div
-      className=" flex bg-brand-main mt-2 px-4 py-2 rounded-lg hover:bg-brand-aux-2 transition duration-300"
+      className=" flex bg-brand-main mt-2 px-4 py-2 rounded-lg hover:bg-brand-aux-2 transition duration-300 whitespace-nowrap overflow-hidden text-fade"
       onClick={handleSelectFile}
     >
       {icon}
-      {title}
+      <span className="">{title}</span>
     </div>
   );
 };
